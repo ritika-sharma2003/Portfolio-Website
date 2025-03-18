bc21202bda36d72d62cdf19e50986e112d71fad4
ritika-sharma2003 <ritusharma3221@gmail.com>
2025-03-18 17:23:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple scroll effect for smooth navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
